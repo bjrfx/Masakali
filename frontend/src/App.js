60903c2a7ea64cbbe6a8f83d752ebf85437539bb
bjrfx <phanikiran1234@gmail.com>
2024-06-27 23:57:36 -0400

    Masakali empty template

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './images/masakali/Logo/sizes/logo512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://masakaliottawa.ca"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Masakali Indian Cuisine
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
